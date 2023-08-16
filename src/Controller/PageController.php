abfa5a30bbdf0a7c95b3ce857a8f6072e11a6b8e
daniel <danielnava901@hotmail.com>
2023-08-16 14:07:25 -0600

    iniciando con base de datos

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,6 +3,8 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,51 +12,67 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 class PageController extends AbstractController
 {
+    #[Route('/')]
+    public function index(Request $request)
+    {
+        return $this->render('index.html.twig',[
+
+        ]);
+    }
 
     #[Route('/login')]
     public function login(Request $request): Response
-
     {
-
         return $this->render('user/login.html.twig',[
 
-
-
-
-
-
         ]);
-
-
     }
+
+
     #[Route('/homepage')]
     public function home(Request $request): Response
-
     {
-
 
         return $this->render('user/base.html.twig',[
 
-
         ]);
-
-
     }
-    #[Route('/movie/{movieId}')]
-    public function movieInfo(Request $request,$movieId): Response
 
+
+    #[Route('/movie/{movieId}')]
+    public function movieInfo(Request $request, $movieId): Response
     {
 
         return $this->render('user/movieinfo.html.twig',[
-            "movieId"=>$movieId
-
-
-
-
-
+            "movieId" => $movieId
         ]);
-
-
     }
 
+
+    #[Route('/checkLogin')]
+    public function checkLogin(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $email = $request->get("email", null);
+        $password = $request->get("password", null);
+
+        $user = $entityManager->getConnection()->fetchOne("
+            SELECT password
+            FROM user
+            WHERE email = :email
+            LIMIT 1
+        ", [
+            "email" => $email
+        ]);
+
+        if($user["password"] == $password) {
+            return $this->render('user/homepage.html.twig',[
+
+            ]);
+        }
+
+        return $this->render('user/login.html.twig',[
+            "error" => "Usuario no existe"
+        ]);
+
+    }
 }
