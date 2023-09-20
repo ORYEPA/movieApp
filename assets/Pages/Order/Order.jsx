7c488d7d54f6621016a5e74e1577c2697e87c8b6
Andrea Peyro <aidenyorep@gmail.com>
2023-09-20 12:27:47 -0600

    --

--- assets/Pages/Order/Order.jsx
+++ assets/Pages/Order/Order.jsx
@@ -2,24 +2,26 @@ import {createRoot} from "react-dom/client";
 import React, {useEffect, useState} from "react";
 import Header from './components/Header';
 import ProductCard from "./components/ProductCard";
+import PayBotton from "./components/PayBotton";
 
 const Order = () => {
     const [productos, setProductos] = useState([]);
 
+    
     //Hace peticion ajax para traer los productos
-    const getProducts = () => {
-          return [
-              {
-                id: 1,
-                tittle: "televison",
-                price: 5
-              },
-              {
-                id: 2,
-                tittle: "Ipad",
-                price: 50
-              },
-         ];
+    const getProducts = ()=> {
+
+        const settings = {
+
+            url:'/cartProducts',
+            data:{
+            }
+        }
+        $.ajax(settings).done(function (response) {
+            var productos=response.productos
+            setProductos(productos);
+
+        })
     }
 
     useEffect(() => {
--- assets/Pages/Order/components/ProductCard.jsx
+++ assets/Pages/Order/components/ProductCard.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const ProductCard = ({producto}) => {
 
-    return <div className="border rounded flex w-full">
-        <div className="p-4 w-3/12">{producto.id}</div>
-        <div className="p-4 w-8/12 border-r">{producto.tittle}</div>
-        <div className="p-4 w-1/12 border-r">${producto.price}</div>
+    return <div>
+        {producto.id} - {producto.tittle} - ${producto.price}
     </div>
 }
 
